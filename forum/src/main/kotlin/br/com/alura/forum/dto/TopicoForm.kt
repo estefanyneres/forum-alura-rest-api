3a0d8756ce5933740f60c9059ff02d2490215e72
Estefany Neres <estefanyyneres@gmail.com>
2025-10-06 22:48:53 -0300

    feat: cria tratamento de erros

--- forum/src/main/kotlin/br/com/alura/forum/dto/TopicoForm.kt
+++ forum/src/main/kotlin/br/com/alura/forum/dto/TopicoForm.kt
@@ -5,11 +5,11 @@ import jakarta.validation.constraints.NotNull
 import jakarta.validation.constraints.Size
 
 data class TopicoForm (
-        @field:NotEmpty
-        @field:Size(min = 5, max = 100)
+        @field:NotEmpty(message = "Titulo nao pode ser vazio")
+        @field:Size(min = 5, max = 100, message = "Titulo deve ter entre 5 e 100 caracteres")
         val titulo: String,
 
-        @field:NotEmpty
+        @field:NotEmpty(message = "Mensagem nao pode ser vazia")
         val mensagem: String,
 
         @field:NotNull
